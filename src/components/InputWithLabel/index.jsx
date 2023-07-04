e582c9f7aea5c75ab1488986fef80488183f5ed2
mikhael zimmermann <mikhael928@gmail.com>
2023-07-04 18:13:38 +0200

    Shit class

--- src/components/InputWithLabel/index.jsx
+++ src/components/InputWithLabel/index.jsx
@@ -1,31 +1,27 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
-const InputWithLabel = ({
-  id,
-  value,
-  type = 'text',
-  onInputChange,
-  isFocused,
-  children,
-}) => {
-  const inputRef = React.useRef();
-  React.useEffect(() => {
-    if (isFocused && inputRef.current) {
-      inputRef.current.focus();
-    }
-  }, [isFocused]);
-  return (
-    <>
-      <label htmlFor={id}>{children}</label>
-      &nbsp;
-      <input
-        ref={inputRef}
-        id={id}
-        type={type}
-        value={value}
-        onChange={onInputChange}
-      />
-    </>
-  );
-};
+class InputWithLabel extends React.Component {
+  render() {
+    const {
+      id,
+      value,
+      type = 'text',
+      onInputChange,
+      children,
+    } = this.props;
+
+    return (
+      <>
+        <label htmlFor={id}>{children}</label>
+        &nbsp;
+        <input
+          id={id}
+          type={type}
+          value={value}
+          onChange={onInputChange}
+        />
+      </>
+    );
+  }
+}
 export default InputWithLabel;
